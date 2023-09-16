2b78b6d251ca8e5f1d04c4aa23d910e76ae3bdc6
Developer-2017 <65723694+Developer-2017@users.noreply.github.com>
2023-09-16 13:28:41 +0530

    Create Header (#1)
    * Create Header
    
    Add Header
    
    * Add Package File
    
    * Add Css File
    
    * Compelete COde od src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,30 @@
-import { AppBar, Toolbar, IconButton, Typography, useTheme } from '@mui/material';
-import { Menu as MenuIcon } from '@mui/material';
+import * as React from 'react';
+import {Grid, Box, Typography} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import '../App.css';
+//import garima from  "../Images/Garima.png";
 
 export const Header = () => {
-  const theme = useTheme();
+  const { t } = useTranslation();
   return (
-    <AppBar
-      sx={{ backgroundColor: 'primary.dark', color: 'secondary.contrastText' }}
-    >
-      <Toolbar>
-        <Typography variant="h6" sx={{ margin: 'auto' }}>
-          Header
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+    <Box className="App-header-banner">
+    <Box className="container">
+    <Grid container spacing={6}>
+    <Grid item md={6}>
+    <Typography variant="h2" gutterBottom>
+       {[t("Hi, I'm John Watson")].join(" ")}
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    {[t("Highly recognized in the community, I can meet the challenges that your company will entrust me. I am a recognized expert in the field of Software Development.")].join(" ")}
+    </Typography>
+    </Grid>
+    <Grid item md={6}>
+    {/* <img src={garima} className='App-logo' alt="logo" /> */}
+    </Grid>
+    </Grid>
+    </Box>
+    </Box>
+  ); 
 };
 
 export default Header;
